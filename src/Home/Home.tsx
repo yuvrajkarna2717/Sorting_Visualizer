cf3bc47ade92f2786d46fdbfd6d77e58017db581
Yuvraj Karna <yuvrajkarna2000@gmail.com>
2025-04-19 22:39:27 +0530

    Fix on mount array to sort was empty so we need to reset array animation function

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Select } from "../components/Input/Select";
 import { Slider } from "../components/Input/Slider";
 import { useSortingAlgorithmContext } from "../context/Visualizer";
@@ -40,6 +41,11 @@ export default function Home() {
       runAnimation
     );
   };
+
+  // on mount array animation should be reset
+  useEffect(() => {
+    resetArrayAndAnimation();
+  }, [])
 
   return (
     <main className="absolute top-0 h-screen w-screen z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
